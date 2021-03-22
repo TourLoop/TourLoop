9bc2f7959cf9b33cc868f0b2fd47137aa91fca10
Kai Bailey <kbailey1@ualberta.ca>
2021-03-22 10:40:39 -0600

    Set up flask api

---
+++ server/flask_api/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, send_from_directory
+
+
+def create_app():
+    # /client/build should hold the built react frontend (npm build)
+    app = Flask(__name__, instance_relative_config=True,
+                static_folder='../../client/build', static_url_path='/')
+
+    # Check that the instance folder exists. It should hold the config.py file.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.config.from_pyfile('config.py', silent=True)
+
+    from flask_api import db
+    db.init_app(app)
+
+    from flask_api import all_paths
+    app.register_blueprint(all_paths.bp)
+
+    # Serves the single page react app from the build folder
+    @app.route('/')
+    def index():
+        return app.send_static_file('index.html')
+
+    return app
