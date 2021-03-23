f5bdde1fcfeaa248e6f4946aa0084dffc2032d27
arostron <alex.rostron24@gmail.com>
2021-03-22 19:41:46 -0600

    Switch to docker-compose in rebuild script

--- database/rebuild-and-run-docker.sh
+++ database/rebuild-and-run-docker.sh
@@ -10,17 +10,15 @@ echo "starting container construction"
 mv osm-ways.csv docker-stuff/csv-files/
 mv osm-nodes.csv docker-stuff/csv-files/
 
+echo "Stop any existing db containers and reubild one (~3 min?)"
 cd docker-stuff
-echo "building container (~3 min?)"
-time ./build.sh
+docker-compose down
+docker-compose up --build -d
 
 echo "compress csv files for server export"
 mkdir -p ../server/instance || true
 tar -cvzf tourloop-database.tar.gz csv-files/*.csv
 mv tourloop-database.tar.gz ../../server/instance
-
-echo "container built! starting container"
-./run.sh
 
 echo "container should be running..."
 echo "localhost:7474 will be available within the next 30s"
