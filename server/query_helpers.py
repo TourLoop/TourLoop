d961e56395068456a5273f4d75a62f46453a443c
arostron <alex.rostron24@gmail.com>
2021-03-30 16:35:24 -0600

    Return full node dictionary in closest point query helpers

--- server/query_helpers.py
+++ server/query_helpers.py
@@ -23,28 +23,26 @@ def get_neo4j_driver():
 
 def record_to_lat_lon_dict(record):
     # TODO: handle record keys len!=1 and name != 'n'
-    return {'lat':record.data()['n']['lat'], 'lon': record.data()['n']['lon']}
+    return {'lat':record['lat'], 'lon': record['lon']}
 
 
 # TOURLOOP FR3 : Closest Node point
 def closest_point(session, lat_string, lon_string):
     """find closest lat long point in db given a lat long string
-    >>> closest_point(get_neo4j_driver().session(), '53.509905', '-113.541233')
+    >>> record_to_lat_lon_dict(closest_point(get_neo4j_driver().session(), '53.509905', '-113.541233'))
     {'lat': 53.5714699, 'lon': -113.6278968}
     """
     #print("finding closest point...")
     res = session.run(closest_point_query, lat=lat_string, lon=lon_string)
-    record = res.single()
-    return record_to_lat_lon_dict(record)
+    return res.single().data()['n']
 
 # TOURLOOP FR3 : Closest Node point
 def closest_point_to_pathtype(session, pathtype, lat_string, lon_string):
     # TODO: pass docstring test
     """find closest lat long point in db given a lat long string
-    >>> closest_point_to_pathtype(get_neo4j_driver().session(), "bike", '53.509905', '-113.541233')
+    >>> record_to_lat_lon_dict(closest_point_to_pathtype(get_neo4j_driver().session(), "bike", '53.509905', '-113.541233'))
     {'lat': 53.5098266, 'lon': -113.5411793}
     """
     #print("finding closest point...")
     res = session.run(closest_point_to_pathtype_query, pathtype=pathtype, lat=lat_string, lon=lon_string)
-    record = res.single()
-    return record_to_lat_lon_dict(record)
+    return res.single().data()['n']
