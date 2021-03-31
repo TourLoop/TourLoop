af48bc6c37c28375c4bd1f9c927cb26841a0fdfc
Kai Bailey <kbailey1@ualberta.ca>
2021-03-31 12:41:46 -0600

    Add toggle all paths and toggle all bike paths to UI

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,65 @@
+import { useState, useEffect } from 'react';
+import { decode } from '@googlemaps/polyline-codec';
+
 import Map from './Map';
 import Sidebar from './Sidebar';
 
+const ALLPATHS = "ALLPATHS";
+const ALLBIKEPATHS = "ALLBIKEPATHS";
+
 function App() {
+  // polylines is of type
+  // [{
+  //    path: [{lat: float, lng: float}, ...],
+  //    displayName: string,
+  //    id: string,
+  //    display: bool,
+  //    color: string,
+  //    time: string,
+  // }, ...]
+  const [polylines, setPolylines] = useState([]);
+
+  const fetchAllPaths = (bikesOnly = false) => {
+    let id = ALLPATHS
+    let url = "/api/allpaths"
+    let pathColor = "#577590"
+    if (bikesOnly) {
+      id = ALLBIKEPATHS
+      url = "/api/allbikepaths"
+      pathColor = "#4BA973"
+    }
+
+
+    let ind = polylines.findIndex(p => p.id === id)
+
+    if (ind !== -1) {
+      // We have already fetched all paths. Turn display on or off.
+      polylines[ind].display = !polylines[ind].display
+    } else {
+      fetch(url)
+        .then(res => res.text())
+        .then((f) => {
+          let latLngs = [];
+          f.split('\n').forEach(function (path) {
+            latLngs.push({ "path": [], display: true, id: ALLPATHS, color: pathColor })
+            var p = decode(path, 6)
+            for (let j = 0; j < p.length; j++) {
+              latLngs[latLngs.length - 1].path.push({ lat: p[j][0], lng: p[j][1] });
+            }
+          })
+          setPolylines(latLngs)
+        },
+          (error) => {
+            console.log(error)
+          }
+        )
+    }
+  }
+
   return (
     <div className='app'>
-      <Map />
-      <Sidebar />
+      <Map polylines={polylines} />
+      <Sidebar fetchAllPaths={fetchAllPaths} />
     </div>
   );
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,29 +1,28 @@
 const Sidebar = props => {
-  const handleSubmit = () => {};
+  const handleSubmit = () => { };
   const downloadDatabaseFiles = () => {
     fetch("/export/tar")
-    .then((response) => response.blob())
-    .then((blob) => {
-      // from https://medium.com/yellowcode/download-api-files-with-react-fetch-393e4dae0d9e
-      // 2. Create blob link to download
-      const url = window.URL.createObjectURL(new Blob([blob]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'tourloop-database-files.tar.gz');
-      // 3. Append to html page
-      document.body.appendChild(link);
-      // 4. Force download
-      link.click();
-      // 5. Clean up and remove the link
-      link.parentNode.removeChild(link);
-    }).catch((error) => {
-      console.log(error)
-    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        // from https://medium.com/yellowcode/download-api-files-with-react-fetch-393e4dae0d9e
+        // 2. Create blob link to download
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'tourloop-database-files.tar.gz');
+        // 3. Append to html page
+        document.body.appendChild(link);
+        // 4. Force download
+        link.click();
+        // 5. Clean up and remove the link
+        link.parentNode.removeChild(link);
+      }).catch((error) => {
+        console.log(error)
+      })
   }
 
   return (
     <div className='sidebar'>
-      <button onClick={downloadDatabaseFiles}>Download Database Files</button>
       <h1 className='sidebar-header'>Generate Routes</h1>
       <form
         onSubmit={handleSubmit}
@@ -57,6 +56,25 @@ const Sidebar = props => {
           Generate Routes
         </button>
       </form>
+      <hr />
+      <h1 className='sidebar-header'>Additional Functionality</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', padding: '2rem' }}>
+        <label >Display All Paths</label>
+        <input
+          id='allPaths'
+          type='checkbox'
+          className='rounded text-blue-500 mb-4'
+          onClick={() => props.fetchAllPaths(false)}
+        />
+        <label>Display All Bike Paths</label>
+        <input
+          id='allBikePaths'
+          type='checkbox'
+          className='rounded text-blue-500 mb-4'
+          onClick={() => props.fetchAllPaths(true)}
+        />
+        <button className='button' onClick={downloadDatabaseFiles}>Download Database</button>
+      </div>
     </div>
   );
 };
