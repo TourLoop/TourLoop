12831f2d31c91eb13b9c121c427231b7dd2f6681
Kai Bailey <kbailey1@ualberta.ca>
2021-04-02 17:46:10 -0600

    modify db_wrapper queries to return nodes

--- server/db_wrapper.py
+++ server/db_wrapper.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase, basic_auth
+from node import *
 
 
 class DBWrapper:
@@ -19,10 +20,8 @@ class DBWrapper:
         """
 
         with self.driver.session() as session:
-            res = session.read_transaction(self._getClosestPoint,
-                                           lat_string, lon_string)
-
-        return record_to_lat_lon_dict(res)
+            return session.read_transaction(self._getClosestPoint,
+                                            lat_string, lon_string)
 
     @staticmethod
     def _getClosestPoint(tx, lat_string, lon_string):
@@ -34,7 +33,8 @@ class DBWrapper:
         limit 1
         """
         res = tx.run(closest_point_query, lat=lat_string, lon=lon_string)
-        return res.single()
+        r = res.single()
+        return Node(None, r.data()['n']['nodeId'], r.data()['n']['lat'], r.data()['n']['lon'])
 
     # TOURLOOP FR3 : Closest Node point
     def getClosestPointToPathtype(self, path_string, lat_string, lon_string):
@@ -46,9 +46,8 @@ class DBWrapper:
         """
 
         with self.driver.session() as session:
-            res = session.read_transaction(
+            return session.read_transaction(
                 self._getClosestPointToPathtype, path_string, lat_string, lon_string)
-        return record_to_lat_lon_dict(res)
 
     @staticmethod
     def _getClosestPointToPathtype(tx, path_string, lat_string, lon_string):
@@ -61,7 +60,35 @@ class DBWrapper:
         """
         res = tx.run(closest_point_to_pathtype_query,
                      pathtype=path_string, lat=lat_string, lon=lon_string)
-        return res.single()
+        r = res.single()
+        return Node(None, r.data()['n']['nodeId'], r.data()['n']['lat'], r.data()['n']['lon'])
+
+    def getNeighbours(self, prev_node):
+        """returns the node_id of the neighbouring nodes connected by a way
+
+        >>> d=DBWrapper("bolt://localhost:7687", "neo4j", "test")
+        >>> d.getNeighbours("2815578994")
+        ['2815578992', '2815578985']
+        """
+
+        with self.driver.session() as session:
+            return session.read_transaction(
+                self._getNeighbours, prev_node)
+
+    @staticmethod
+    def _getNeighbours(tx, prev_node):
+        closest_point_to_pathtype_query = """
+        Match(n:Node {nodeId: $id})-[:Way]-(n1:Node)
+        Return n1
+        """
+        res = tx.run(closest_point_to_pathtype_query,
+                     id=prev_node.node_id)
+        nodes = []
+        for r in res:
+            nodes.append(Node(prev_node, r.data()['n1']['nodeId'], r.data()[
+                         'n1']['lat'], r.data()['n1']['lon']))
+
+        return nodes
 
     def getPinsExampleRoutes(self):
         pins_query = """
