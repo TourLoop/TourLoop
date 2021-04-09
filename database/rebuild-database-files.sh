076fe14c248e4473b4bfe41d23150afa8871904b
arostron <alex.rostron24@gmail.com>
2021-04-08 22:08:07 -0600

    Fix rebuild database files directory bug

--- database/rebuild-database-files.sh
+++ database/rebuild-database-files.sh
@@ -13,6 +13,7 @@ mv osm-ways.csv docker-stuff/database-files/
 mv osm-nodes.csv docker-stuff/database-files/
 
 echo "compress csv files for server export"
+cd docker-stuff
 mkdir -p ../server/instance || true
 tar -cvzf tourloop-database.tar.gz database-files/*
 mv tourloop-database.tar.gz ../../server/instance
@@ -20,3 +21,4 @@ mv tourloop-database.tar.gz ../../server/instance
 echo "save all path and all bike path results"
 mv ../all_dirt_paths.txt ../../server/instance
 mv ../all_bike_paths.txt ../../server/instance
+cd ../
