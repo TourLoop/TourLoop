93886d13ae5a450facf907f0a7a4ee849d6d1cc4
arostron <alex.rostron24@gmail.com>
2021-04-11 14:19:48 -0600

    Refactor rebuild-and-run script to call rebuild database files, then cd back and docker-compose

--- database/rebuild-and-run-docker.sh
+++ database/rebuild-and-run-docker.sh
@@ -5,29 +5,10 @@
 # fail if one command fails
 set -e
 
-echo "parsing xml file (~3 min?)"
-time python3 osm_xml_to_csv.py
+./rebuild-database-files.sh
 
-echo "starting container construction"
-mv osm-ways.csv docker-stuff/database-files/
-mv osm-nodes.csv docker-stuff/database-files/
-
-# TODO: refactor script to account for new docker-compose location
 echo "Stop any existing db containers and reubild one (~3 min?)"
-cd docker-stuff
+cd ../ # cd to top level
 docker-compose down
 docker-compose up --build -d
-
-echo "compress csv files for server export"
-mkdir -p ../server/instance || true
-tar -cvzf tourloop-database.tar.gz database-files/*
-mv tourloop-database.tar.gz ../../server/instance
-
-echo "save all path and all bike path results"
-mv ../all_dirt_paths.txt ../../server/instance
-mv ../all_bike_paths.txt ../../server/instance
-mv ../all_paved_paths.txt ../../server/instance
-
-echo "container should be running..."
-echo "localhost:7474 will be available within the next 30s"
 
