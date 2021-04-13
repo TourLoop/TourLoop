61c2bba35c97637f7bcf31a374be9abdba7d9908
David Herczeg <david.herczeg@live.ca>
2021-04-12 20:26:07 -0600

    Move navigation styles to stylesheet

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Navigation = props => {
   const { children } = props;
 
-  return <div className='mb-4 px-2 py-1 flex'>{children}</div>;
+  return <div className='navigation'>{children}</div>;
 };
 
 Navigation.propTypes = {};
--- client/src/components/navigation/NavigationTab.jsx
+++ client/src/components/navigation/NavigationTab.jsx
@@ -4,10 +4,7 @@ import PropTypes from 'prop-types';
 const NavigationTab = props => {
   const { label, onClick } = props;
   return (
-    <button
-      className='flex-1 mx-2 h-12 bg-indigo-500 shadow-lg rounded-lg  font-bold text-white text-sm'
-      onClick={onClick}
-    >
+    <button className='navigation-tab' onClick={onClick}>
       {label}
     </button>
   );
