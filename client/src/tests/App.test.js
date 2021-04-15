4ab7dd2a11e078a1e97a2d8392040ca52e7f4295
David Herczeg <david.herczeg@live.ca>
2021-04-15 14:58:44 -0600

    Setup enzyme and create two simple tests for App component

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from '../components/App';
+import Map from '../components/Map.jsx';
+import Sidebar from '../components/Sidebar.jsx';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders one <Map /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Map)).toHaveLength(1);
+  });
+
+  it('renders one <Sidebar /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Sidebar)).toHaveLength(1);
+  });
 });
