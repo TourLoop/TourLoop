b6841315442854e1932baa667e788adceccbc0de
Kai Bailey <kbailey1@ualberta.ca>
2021-04-15 11:18:41 -0600

    Removed the Ddisplay all paved paths option

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ const algorithms = {
   allPavedPaths: 'All Paved Paths',
 };
 
-const Sidebar = props => {
+const Sidebar = (props) => {
   const {
     polylines,
     setPolylines,
@@ -66,7 +66,7 @@ const Sidebar = props => {
     }
   }, [endLocation]);
 
-  const handleStartLocation = e => {
+  const handleStartLocation = (e) => {
     const latLng = e.target.value.split(',');
     const coord =
       +latLng[0] && +latLng[1]
@@ -81,7 +81,7 @@ const Sidebar = props => {
     setStartLocation(coord);
   };
 
-  const handleEndLocation = e => {
+  const handleEndLocation = (e) => {
     const latLng = e.target.value.split(',');
     const coord =
       +latLng[0] && +latLng[1]
@@ -96,7 +96,7 @@ const Sidebar = props => {
     setEndLocation(coord);
   };
 
-  const onSubmit = async data => {
+  const onSubmit = async (data) => {
     setLoading(true);
 
     if (!pointToPointChecked) {
@@ -120,7 +120,7 @@ const Sidebar = props => {
       }
 
       const statistics = routeStatistics.filter(
-        rs => rs.algorithm !== route.algorithm
+        (rs) => rs.algorithm !== route.algorithm
       );
       const newRouteStatistics = [
         ...statistics,
@@ -133,12 +133,12 @@ const Sidebar = props => {
       ];
 
       const latLngs = decode(route.path, 6);
-      const paths = latLngs.map(latLng => ({
+      const paths = latLngs.map((latLng) => ({
         lat: latLng[0],
         lng: latLng[1],
       }));
 
-      const newPolylines = polylines.map(polyline =>
+      const newPolylines = polylines.map((polyline) =>
         polyline.id === route.algorithm
           ? {
               paths: [paths],
@@ -160,8 +160,8 @@ const Sidebar = props => {
 
   const downloadDatabaseFiles = () => {
     fetch('/export/tar')
-      .then(response => response.blob())
-      .then(blob => {
+      .then((response) => response.blob())
+      .then((blob) => {
         // from https://medium.com/yellowcode/download-api-files-with-react-fetch-393e4dae0d9e
         // 2. Create blob link to download
         const url = window.URL.createObjectURL(new Blob([blob]));
@@ -175,7 +175,7 @@ const Sidebar = props => {
         // 5. Clean up and remove the link
         link.parentNode.removeChild(link);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
@@ -290,19 +290,19 @@ const Sidebar = props => {
           <Header label='Route Legend' />
           <div className='px-8'>
             {polylines
-              .filter(p => p.paths.length > 0)
-              .map(p => (
+              .filter((p) => p.paths.length > 0)
+              .map((p) => (
                 <Checkbox
                   key={p.id}
                   id={p.id}
                   style={{ color: p.color }}
                   onChange={() => toggleDisplay(p.id)}
-                  checked={polylines.find(poly => poly.id === p.id).display}
+                  checked={polylines.find((poly) => poly.id === p.id).display}
                   label={`Display Route From ${algorithms[p.id]}`}
                 />
               ))}
           </div>
-          {routeStatistics.map(rs => (
+          {routeStatistics.map((rs) => (
             <RouteStatistic
               key={rs.algorithm}
               algorithm={algorithms[rs.algorithm]}
@@ -335,7 +335,8 @@ const Sidebar = props => {
               checked={bikePathsChecked}
               label='Display All Bike Paths'
             />
-            <Checkbox
+            {/* Remove this feature for now as it slows down the frontend. */}
+            {/* <Checkbox
               id='allPavedPaths'
               onChange={() => {
                 setPavedPathsChecked(!pavedPathsChecked);
@@ -343,8 +344,8 @@ const Sidebar = props => {
               }}
               checked={pavedPathsChecked}
               label='Display All Paved Paths'
-            />
-            // TOURLOOP FR22 : Toggle Location tracking
+            /> */}
+            {/* TOURLOOP FR22 : Toggle Location tracking */}
             <Checkbox
               id='locationToggle'
               onChange={() => {
