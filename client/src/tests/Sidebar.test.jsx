eb2598dd29545b5ccdb5426356cb772cde15c2be
David Herczeg <david.herczeg@live.ca>
2021-04-16 00:38:05 -0600

    Test start & end location input value change on blur and on Sidebar component render. Branch coverage on RouteStatistic component

--- client/src/tests/Sidebar.test.jsx
+++ client/src/tests/Sidebar.test.jsx
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import Sidebar from '../components/Sidebar';
 import Navigation from '../components/navigation/Navigation';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('<Sidebar />', () => {
   it('renders one <Navigation /> component', () => {
@@ -9,16 +9,41 @@ describe('<Sidebar />', () => {
     expect(wrapper.find(Navigation)).toHaveLength(1);
   });
 
-  it('changes start & end location input values', () => {
+  it('changes start & end location input values on render', () => {
     render(
       <Sidebar
         startLocation={{ lat: 53.509905, lng: -113.541233 }}
         endLocation={{ lat: 53.509905, lng: -113.541233 }}
+        pointToPointChecked
       />
     );
 
     const startLocationInput = screen.getByLabelText('Start Location');
-    const endLocationInput = screen.getByLabelText('Start Location');
+    const endLocationInput = screen.getByLabelText('End Location');
+
+    expect(startLocationInput.value).toBe('53.509905, -113.541233');
+
+    expect(endLocationInput.value).toBe('53.509905, -113.541233');
+  });
+
+  it('changes start & end location input values on blur', () => {
+    render(
+      <Sidebar
+        startLocation={{ lat: 53.509905, lng: -113.541233 }}
+        endLocation={{ lat: 53.509905, lng: -113.541233 }}
+        setStartLocation={() => {}}
+        setEndLocation={() => {}}
+        pointToPointChecked
+      />
+    );
+
+    const startLocationInput = screen.getByLabelText('Start Location');
+    const endLocationInput = screen.getByLabelText('End Location');
+
+    startLocationInput.focus();
+    startLocationInput.blur();
+    endLocationInput.focus();
+    endLocationInput.blur();
 
     expect(startLocationInput.value).toBe('53.509905, -113.541233');
     expect(endLocationInput.value).toBe('53.509905, -113.541233');
