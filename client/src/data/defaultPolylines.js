8f0e465a23ddaccd50d07cf5f1c7b9d876d64a39
David Herczeg <david.herczeg@live.ca>
2021-04-16 00:07:00 -0600

    Test render polylines in Map component

---
+++ client/src/data/defaultPolylines.js
@@ -0,0 +1,38 @@
+export const defaultPolylines = [
+  {
+    paths: [],
+    display: true,
+    id: 'allDirtPaths',
+    color: '#577590',
+  },
+  {
+    paths: [],
+    display: true,
+    id: 'allBikePaths',
+    color: '#4BA973',
+  },
+  {
+    paths: [],
+    display: true,
+    id: 'allPavedPaths',
+    color: '#f09841',
+  },
+  {
+    paths: [],
+    display: true,
+    id: 'algo1',
+    color: '#FF6347',
+  },
+  {
+    paths: [],
+    display: true,
+    id: 'algo2',
+    color: '#3B82F6',
+  },
+  {
+    paths: [],
+    display: true,
+    id: 'algo3',
+    color: '#EC4899',
+  },
+];
--- client/src/tests/Map.test.jsx
+++ client/src/tests/Map.test.jsx
@@ -1,10 +1,27 @@
 import { shallow } from 'enzyme';
 import Map from '../components/Map';
 import { GoogleMap } from '@react-google-maps/api';
+import { defaultPolylines } from '../data/defaultPolylines';
 
 describe('<Map />', () => {
   it('renders one <GoogleMap /> component', () => {
     const wrapper = shallow(<Map polylines={[]} />);
+    expect(wrapper.find(GoogleMap)).toHaveLength(1);
+  });
+
+  it('renders polylines with at least one non-empty path in polylines prop', () => {
+    const polylines = defaultPolylines.filter(p => p.id !== 'algo1');
+    const newPolylines = [
+      ...polylines,
+      {
+        paths: [[53.509905, -113.541233]],
+        display: true,
+        id: 'algo1',
+        color: '#FF6347',
+      },
+    ];
+    const wrapper = shallow(<Map polylines={newPolylines} />);
+
     expect(wrapper.find(GoogleMap)).toHaveLength(1);
   });
 });
