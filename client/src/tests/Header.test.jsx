6241b512631361bef10db5ad579e7c3bb9b5fd1e
David Herczeg <david.herczeg@live.ca>
2021-04-15 19:01:05 -0600

    Test coverage on Header component

--- client/src/tests/Header.test.jsx
+++ client/src/tests/Header.test.jsx
@@ -1,9 +1,15 @@
 import { shallow } from 'enzyme';
 import Header from '../components/Header';
+import { render, screen } from '@testing-library/react';
 
 describe('<Header />', () => {
   it('renders one <h1 /> element', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.find('h1')).toHaveLength(1);
+  });
+
+  it('renders', () => {
+    render(<Header label='testLabel' />);
+    expect(screen.getByText('testLabel')).toBeTruthy();
   });
 });
